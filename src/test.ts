8df67024eb672bc1a2923e4ba417eb5055200a3f
Vladislav Kamenev <mazahakaforever@ya.ru>
2020-09-13 13:31:05 +0300

    Connect to mongo at test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,14 @@
+import './helpers/env';
+
 import Grabber from "./grabber";
 import { Publisher } from "./publisher";
 import Uploader from "./uploader";
-
+import { connect } from './models';
+const { MONGO } = process.env;
 (async () => {
+    if (!MONGO) throw 'No mongo specified';
+    await connect(MONGO);
     await (new Grabber().process());
     await (new Uploader().start());
-    await new Publisher().process();
+    //await new Publisher().process();
 })();
