6dc5729b225a0413cdf388e23d0e69471a27a3d1
Vladislav Kamenev <mazahakaforever@ya.ru>
2020-11-22 05:55:00 +0300

    Let people sleep at night

--- src/constants/intervals.ts
+++ src/constants/intervals.ts
@@ -4,4 +4,5 @@ const HOUR = 60 * MINUTE;
 
 export const GRABBER_INTERVAL = HOUR;
 export const UPLOADER_INTERVAL = 15 * MINUTE;
-export const PUBLISHER_INTERVAL = '1 */2 * * *';
+export const PUBLISHER_INTERVAL = '1 10-23/2 * * *';
+export const TOP_INTERVAL = '1 23 * * *';
